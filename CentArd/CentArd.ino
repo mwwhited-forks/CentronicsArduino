733039199db1ef7d57ca384dc37baa8ee7920e7a
Matthew Whited <matt@whited.us>
2023-03-17 08:48:23 -0400

    clean up code

--- CentArd/CentArd.ino
+++ CentArd/CentArd.ino
@@ -4,20 +4,59 @@
 // MOSI: pin 11
 // MISO: pin 12
 // CLK:  pin 13
-// CS:   pin 3
+// CS:   pin 4
 
 // LCD pinout:
 // EN: pin A7
 
+#define __BOARD_UNO
+//#define __BOARD_MEGA
+
+#define __ENABLE_LCD
+#define __ENABLE_SDCARD
+//#define __ENABLE_PARALLEL
+
+#if defined(__ENABLE_LCD)
 #include <LiquidCrystal.h>
+#endif
+
+#if defined(__ENABLE_SDCARD)
 #include <SPI.h>
 #include <SD.h>
+#endif
 
 // 10s timeout before considering the print completed
 #define TIMEOUT_MS 10000
 
 // Global variables/flags
+#if defined(__ENABLE_LCD)
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);  // RS, EN, D4, D5, D6, D7
+#endif
+const int serialPortSpeed = 115200;
+
+#if defined(__ENABLE_SDCARD)
+//SD card must only be used when LCD is disabled
+const int sdcardPin = 4;  //SD Card uses SPI pins with CS below 
+#endif
+
+#if defined(__BOARD_UNO)  
+  // const int parallelPortErrorPin = 22; //fixed high
+  // const int parallelPortSelectPin = 24; // fixed high
+  // const int parallelPortPaperOutPin = 26; // fixed low
+  const int parallelPortDataPins[8] = {0,1,2,3,14,15,16,17}; //data in
+  const int parallelPortStrobePin = 18; //on receive
+  const int parallelPortBusyPin = 19; //toggle status
+  const int parallelPortAckPin = 10; //toggle confirm
+#elif defined(__BOARD_MEGA)
+  const int parallelPortDataPins[8] = {39,41,43,45,47,49,46,48}; //data in
+  const int parallelPortStrobePin = 18; //on receive
+  const int parallelPortErrorPin = 22; //fixed high
+  const int parallelPortSelectPin = 24; // fixed high
+  const int parallelPortPaperOutPin = 26; // fixed low
+  const int parallelPortBusyPin = 28; //toggle status
+  const int parallelPortAckPin = 30; //toggle confirm
+#endif
+
 bool init_complete = false;
 bool print_in_progress = false;
 bool data_ready = false;
@@ -25,65 +64,94 @@ byte data = 0;
 byte buff[512];
 int buff_index = 0;
 long last_update;
-File current_file;
 long file_size = 0;
 
-
-void setup() 
-{
-  Serial.begin(115200);
-  delay(10);
-  Serial.println();
-  Serial.println();
-  Serial.println();
-  
-  lcd.begin(16, 2);  
-  delay(10);
-  lcd.clear();  
-  
+#if defined(__ENABLE_SDCARD)  
+File current_file;
+void initSdCard() {
   // Initialize SD card
   Serial.println("Init SD");
-  pinMode(53, OUTPUT);  // HW CS pin, init it just in case
-  pinMode(3, OUTPUT);
-  if ( !SD.begin(3) ) 
+
+  pinMode(sdcardPin, OUTPUT);
+  //pinMode(53, OUTPUT);  // HW CS pin, init it just in case
+  if ( !SD.begin(sdcardPin) ) 
   {
     Serial.println("SD Init Failed");
+
+#if defined(__ENABLE_LCD)    
     lcd.print("! No SD card !");
+#endif
   }
   else
   {
     Serial.println("SD Init Ok");
+
+#if defined(__ENABLE_LCD)
     lcd.print("--== Ready ==--");
+#endif
   }
-  
+}
+#endif
+
+void initSerialPort() {
+  Serial.begin(serialPortSpeed);
+  delay(10);
+  Serial.println();
+  Serial.println();
+  Serial.println();
+}
+
+#if defined(__ENABLE_LCD)
+void initLcdDisplay() {
+  lcd.begin(16, 2);  
+  delay(10);
+  lcd.clear();  
+}
+#endif
+
+void initParallelPort() {
   // Configure pins
-  pinMode(18, INPUT_PULLUP); // Strobe - normally high
-  attachInterrupt(digitalPinToInterrupt(18), StrobeFallingEdge, FALLING); // Attach to pin interrupt
-  
-  pinMode(22, OUTPUT);  // Error - normally high
-  digitalWrite(22, true);
-
-  pinMode(24, OUTPUT);  // Select - normally high
-  digitalWrite(24, true);
-
-  pinMode(26, OUTPUT);  // Paper out - normally low
-  digitalWrite(26, false);
-
-  pinMode(28, OUTPUT);  // Busy - normally low
-  digitalWrite(28, false);
-  
-  pinMode(30, OUTPUT);  // Ack - normally high
-  digitalWrite(30, true);
-
-  pinMode(39, INPUT_PULLUP);  // D0
-  pinMode(41, INPUT_PULLUP);  // D1
-  pinMode(43, INPUT_PULLUP);  // D2
-  pinMode(45, INPUT_PULLUP);  // D3
-  pinMode(47, INPUT_PULLUP);  // D4
-  pinMode(49, INPUT_PULLUP);  // D5
-  pinMode(46, INPUT_PULLUP);  // D6
-  pinMode(48, INPUT_PULLUP);  // D7
+  pinMode(parallelPortStrobePin, INPUT_PULLUP); // Strobe - normally high
+  attachInterrupt(digitalPinToInterrupt(parallelPortStrobePin), StrobeFallingEdge, FALLING); // Attach to pin interrupt
     
+#if !defined(__BOARD_UNO) 
+  pinMode(parallelPortErrorPin, OUTPUT);  // Error - normally high
+  digitalWrite(parallelPortErrorPin, true);
+
+  pinMode(parallelPortSelectPin, OUTPUT);  // Select - normally high
+  digitalWrite(parallelPortSelectPin, true);
+
+  pinMode(parallelPortPaperOutPin, OUTPUT);  // Paper out - normally low
+  digitalWrite(parallelPortPaperOutPin, false);
+#endif
+
+  pinMode(parallelPortBusyPin, OUTPUT);  // Busy - normally low
+  digitalWrite(parallelPortBusyPin, false);
+  
+  pinMode(parallelPortAckPin, OUTPUT);  // Ack - normally high
+  digitalWrite(parallelPortAckPin, true);
+
+  pinMode(parallelPortDataPins[0], INPUT_PULLUP);  // D0
+  pinMode(parallelPortDataPins[1], INPUT_PULLUP);  // D1
+  pinMode(parallelPortDataPins[2], INPUT_PULLUP);  // D2
+  pinMode(parallelPortDataPins[3], INPUT_PULLUP);  // D3
+  pinMode(parallelPortDataPins[4], INPUT_PULLUP);  // D4
+  pinMode(parallelPortDataPins[5], INPUT_PULLUP);  // D5
+  pinMode(parallelPortDataPins[6], INPUT_PULLUP);  // D6
+  pinMode(parallelPortDataPins[7], INPUT_PULLUP);  // D7
+}
+
+void setup() 
+{
+  initSerialPort();
+#if defined(__ENABLE_LCD)
+  initLcdDisplay();  
+#endif
+#if defined(__ENABLE_SDCARD) 
+  initSdCard();
+#endif
+  initParallelPort();
+      
   // Update timeout
   last_update = millis();
   Serial.println("Init Complete");
@@ -103,11 +171,11 @@ void loop()
     data_ready = false;
     
     // Ack byte, reset busy
-    digitalWrite(30, false);  // ACK
+    digitalWrite(parallelPortAckPin, false);  // ACK
     delayMicroseconds(7);
-    digitalWrite(28, false);  // BUSY
+    digitalWrite(parallelPortBusyPin, false);  // BUSY
     delayMicroseconds(5);
-    digitalWrite(30, true);   // ACK
+    digitalWrite(parallelPortAckPin, true);   // ACK
 
     // Reset timeout
     last_update = millis();
@@ -116,14 +184,20 @@ void loop()
     if (!print_in_progress)
     {
       // Just started printing. Create new file
+#if defined(__ENABLE_SDCARD) 
       CreateNewFile();
+#endif
       Serial.print("Receiving from printer.");
       file_size = 0;
 
+#if defined(__ENABLE_LCD)
       // Update LCD
       lcd.clear();
       lcd.print("Prn to:");
+#if defined(__ENABLE_SDCARD) 
       lcd.print(current_file.name());      
+#endif  
+#endif
     }
     print_in_progress = true;
   }
@@ -133,15 +207,19 @@ void loop()
   {
     // Flush buffer to file
     Serial.print(".");
-    WriteToFile(buff, sizeof(buff));
+#if defined(__ENABLE_SDCARD) 
+    WriteToFile(buff, sizeof(buff));   
+#endif
     file_size += buff_index - 1;
     buff_index = 0;    
 
+#if defined(__ENABLE_LCD)
     // Update LCD
     lcd.setCursor(0, 1);
     lcd.print("Size:");
     lcd.print(file_size);
-    lcd.print("B");
+    lcd.print("B");   
+#endif
   }
 
   // Timeout
@@ -150,24 +228,35 @@ void loop()
     // Timeout. Flush the buffer to file
     if (buff_index > 0)
     {
+#if defined(__ENABLE_SDCARD) 
       WriteToFile(buff, buff_index - 1);
+#endif
       file_size += buff_index - 1;
       buff_index = 0;
     }
     Serial.println(".Done");
     Serial.print("Closing file..");
+
+#if defined(__ENABLE_SDCARD) 
     current_file.close();
+#endif
+    
     Serial.println("..Ok");
     print_in_progress = false;
 
+#if defined(__ENABLE_LCD)
     // Update LCD
     lcd.clear();
     lcd.print("Done: ");
+#if defined(__ENABLE_SDCARD) 
     lcd.print(current_file.name());
+#endif
+    //TODO: should we look at header and rename file?
+#endif
   } 
 }
 
-
+#if defined(__ENABLE_SDCARD) 
 void CreateNewFile()
 {  
   // Find unique file 
@@ -186,7 +275,6 @@ void CreateNewFile()
   Serial.println(fname);
 }
 
-
 void WriteToFile(byte* b, int b_size)
 {
   // Verify that the file is open
@@ -200,7 +288,7 @@ void WriteToFile(byte* b, int b_size)
     Serial.println("Can't write to file");
   }
 }
-
+#endif
 
 // Strobe pin on falling edge interrupt
 void StrobeFallingEdge()
@@ -212,19 +300,18 @@ void StrobeFallingEdge()
     }
     
   // Set busy signal
-  digitalWrite(28, true);
+  digitalWrite(parallelPortBusyPin, true);
   
   // Read data from port
-  data = (digitalRead(39) << 0) | 
-         (digitalRead(41) << 1) | 
-         (digitalRead(43) << 2) | 
-         (digitalRead(45) << 3) |
-         (digitalRead(47) << 4) |
-         (digitalRead(49) << 5) |
-         (digitalRead(46) << 6) |
-         (digitalRead(48) << 7);
+  data = (digitalRead(parallelPortDataPins[0]) << 0) | 
+         (digitalRead(parallelPortDataPins[1]) << 1) | 
+         (digitalRead(parallelPortDataPins[2]) << 2) | 
+         (digitalRead(parallelPortDataPins[3]) << 3) |
+         (digitalRead(parallelPortDataPins[4]) << 4) |
+         (digitalRead(parallelPortDataPins[5]) << 5) |
+         (digitalRead(parallelPortDataPins[6]) << 6) |
+         (digitalRead(parallelPortDataPins[7]) << 7);
   
   // Set ready bit
   data_ready = true;    
 }
-
