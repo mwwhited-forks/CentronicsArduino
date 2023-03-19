9e0a992e56dd329d1b5d3612c02b4fa6536e5a0f
Matthew Whited <matt@whited.us>
2023-03-19 18:47:04 -0400

    upgraded to sdfat and software spi

--- CentArd/CentArd.ino
+++ CentArd/CentArd.ino
@@ -1,5 +1,6 @@
+
 // Arduino Centronics printer capture tool 
-// Using Ethernet shield with built-in SD card
+
 // SD card pinout:
 // MOSI: pin 11
 // MISO: pin 12
@@ -7,23 +8,47 @@
 // CS:   pin 4
 
 // LCD pinout:
-// EN: pin A7
+//  Reset  8
+//  Enable 9
+//  Data0  4
+//  Data1  5
+//  Data2  6
+//  Data3  7
 
-#define __BOARD_UNO
-//#define __BOARD_MEGA
+// Parallel Printer Pinout:
+//  
+// // const int parallelPortErrorPin = 22; //fixed high
+// // const int parallelPortSelectPin = 24; // fixed high
+// // const int parallelPortPaperOutPin = 26; // fixed low
+// const int parallelPortDataPins[8] = {0,1,2,3,14,15,16,17}; //data in
+// const int parallelPortStrobePin = 18; //on receive
+// const int parallelPortBusyPin = 19; //toggle status
+// const int parallelPortAckPin = 10; //toggle confirm
 
-#define __ENABLE_LCD
-#define __ENABLE_SDCARD
-//#define __ENABLE_PARALLEL
-
-#if defined(__ENABLE_LCD)
 #include <LiquidCrystal.h>
+#include "SdFat.h"
+
+#if SPI_DRIVER_SELECT != 2 
+#error SPI_DRIVER_SELECT must be two in SdFat/SdFatConfig.h
 #endif
 
-#if defined(__ENABLE_SDCARD)
-#include <SPI.h>
-#include <SD.h>
-#endif
+// SD_FAT_TYPE = 0 for SdFat/File as defined in SdFatConfig.h,
+// 1 for FAT16/FAT32, 2 for exFAT, 3 for FAT16/FAT32 and exFAT.
+#define SD_FAT_TYPE 0
+
+const uint8_t SD_CS_PIN = 4;
+//
+// Pin numbers in templates must be constants.
+const uint8_t SOFT_MISO_PIN = 12;
+const uint8_t SOFT_MOSI_PIN = 11;
+const uint8_t SOFT_SCK_PIN  = 13;
+
+// SdFat software SPI template
+SoftSpiDriver<SOFT_MISO_PIN, SOFT_MOSI_PIN, SOFT_SCK_PIN> softSpi;
+#define SD_CONFIG SdSpiConfig(SD_CS_PIN, DEDICATED_SPI, SD_SCK_MHZ(0), &softSpi)
+
+SdFat sd;
+File current_file;
 
 // 10s timeout before considering the print completed
 #define TIMEOUT_MS 10000
@@ -40,85 +65,67 @@ int buff_index = 0;
 long file_size = 0;
 
 // Global variables/flags
-#if defined(__ENABLE_LCD)
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);  // RS, EN, D4, D5, D6, D7
-#endif
 
-#if defined(__ENABLE_SDCARD)
-//SD card must only be used when LCD is disabled
-const int sdcardPin = 4;  //SD Card uses SPI pins with CS below 
-#endif
+const int parallelPortDataPins[8] = {39,41,43,45,47,49,46,48}; //data in
+const int parallelPortStrobePin = 18; //on receive
+const int parallelPortErrorPin = 22; //fixed high
+const int parallelPortSelectPin = 24; // fixed high
+const int parallelPortPaperOutPin = 26; // fixed low
+const int parallelPortBusyPin = 28; //toggle status
+const int parallelPortAckPin = 30; //toggle confirm
 
-#if defined(__ENABLE_PARALLEL)
-#if defined(__BOARD_UNO)  
-  // const int parallelPortErrorPin = 22; //fixed high
-  // const int parallelPortSelectPin = 24; // fixed high
-  // const int parallelPortPaperOutPin = 26; // fixed low
-  const int parallelPortDataPins[8] = {0,1,2,3,14,15,16,17}; //data in
-  const int parallelPortStrobePin = 18; //on receive
-  const int parallelPortBusyPin = 19; //toggle status
-  const int parallelPortAckPin = 10; //toggle confirm
-#elif defined(__BOARD_MEGA)
-  const int parallelPortDataPins[8] = {39,41,43,45,47,49,46,48}; //data in
-  const int parallelPortStrobePin = 18; //on receive
-  const int parallelPortErrorPin = 22; //fixed high
-  const int parallelPortSelectPin = 24; // fixed high
-  const int parallelPortPaperOutPin = 26; // fixed low
-  const int parallelPortBusyPin = 28; //toggle status
-  const int parallelPortAckPin = 30; //toggle confirm
-#endif
-#endif
-
-#if defined(__ENABLE_SDCARD)  
-File current_file;
 void initSdCard() {
   // Initialize SD card
   Serial.println("Init SD");
+    lcd.clear();
+    lcd.print("--= Loading =--");
 
-  pinMode(sdcardPin, OUTPUT);
-  //pinMode(53, OUTPUT);  // HW CS pin, init it just in case
-  if ( !SD.begin(sdcardPin) ) 
+  pinMode(SD_CS_PIN, OUTPUT);
+  if (!sd.begin(SD_CONFIG)) 
   {
     Serial.println("SD Init Failed");
-
-#if defined(__ENABLE_LCD)    
+    lcd.clear();
     lcd.print("! No SD card !");
-#endif
+    sd.initErrorHalt();
   }
   else
   {
     Serial.println("SD Init Ok");
-
-#if defined(__ENABLE_LCD)
+    lcd.clear();
     lcd.print("--== Ready ==--");
-#endif
   }
 }
-#endif
 
 void initSerialPort() {
+
+  // // Wait for USB Serial
+  // while (!Serial) {
+  //   yield();
+  // }
+  // Serial.println("Type any character to start");
+  // while (!Serial.available()) {
+  //   yield();
+  // }
+
   Serial.begin(serialPortSpeed);
   delay(10);
+  Serial.println("serial init");
   Serial.println();
   Serial.println();
   Serial.println();
 }
 
-#if defined(__ENABLE_LCD)
 void initLcdDisplay() {
   lcd.begin(16, 2);  
   delay(10);
   lcd.clear();  
 }
-#endif
-
-#if defined(__ENABLE_PARALLEL)
 void initParallelPort() {
   // Configure pins
   pinMode(parallelPortStrobePin, INPUT_PULLUP); // Strobe - normally high
   attachInterrupt(digitalPinToInterrupt(parallelPortStrobePin), StrobeFallingEdge, FALLING); // Attach to pin interrupt
-    
-#if !defined(__BOARD_UNO) 
+
   pinMode(parallelPortErrorPin, OUTPUT);  // Error - normally high
   digitalWrite(parallelPortErrorPin, true);
 
@@ -127,7 +134,6 @@ void initParallelPort() {
 
   pinMode(parallelPortPaperOutPin, OUTPUT);  // Paper out - normally low
   digitalWrite(parallelPortPaperOutPin, false);
-#endif
 
   pinMode(parallelPortBusyPin, OUTPUT);  // Busy - normally low
   digitalWrite(parallelPortBusyPin, false);
@@ -144,20 +150,13 @@ void initParallelPort() {
   pinMode(parallelPortDataPins[6], INPUT_PULLUP);  // D6
   pinMode(parallelPortDataPins[7], INPUT_PULLUP);  // D7
 }
-#endif
 
 void setup() 
 {
   initSerialPort();
-#if defined(__ENABLE_LCD)
   initLcdDisplay();  
-#endif
-#if defined(__ENABLE_SDCARD) 
   initSdCard();
-#endif
-#if defined(__ENABLE_PARALLEL)
   initParallelPort();
-#endif
       
   // Update timeout
   last_update = millis();
@@ -177,14 +176,12 @@ void loop()
     // Reset data ready flag
     data_ready = false;
     
-#if defined(__ENABLE_PARALLEL)
     // Ack byte, reset busy
     digitalWrite(parallelPortAckPin, false);  // ACK
     delayMicroseconds(7);
     digitalWrite(parallelPortBusyPin, false);  // BUSY
     delayMicroseconds(5);
     digitalWrite(parallelPortAckPin, true);   // ACK
-  #endif
 
     // Reset timeout
     last_update = millis();
@@ -193,20 +190,17 @@ void loop()
     if (!print_in_progress)
     {
       // Just started printing. Create new file
-#if defined(__ENABLE_SDCARD) 
       CreateNewFile();
-#endif
       Serial.print("Receiving from printer.");
       file_size = 0;
 
-#if defined(__ENABLE_LCD)
+      char nameBuffer[20];
+      current_file.getName(nameBuffer, 20);
+
       // Update LCD
       lcd.clear();
       lcd.print("Prn to:");
-#if defined(__ENABLE_SDCARD) 
-      lcd.print(current_file.name());      
-#endif  
-#endif
+      lcd.print(nameBuffer);      
     }
     print_in_progress = true;
   }
@@ -216,19 +210,15 @@ void loop()
   {
     // Flush buffer to file
     Serial.print(".");
-#if defined(__ENABLE_SDCARD) 
     WriteToFile(buff, sizeof(buff));   
-#endif
     file_size += buff_index - 1;
     buff_index = 0;  
 
-#if defined(__ENABLE_LCD)
     // Update LCD
     lcd.setCursor(0, 1);
     lcd.print("Size:");
     lcd.print(file_size);
     lcd.print("B");   
-#endif
   }  
 
   // Timeout
@@ -237,35 +227,33 @@ void loop()
     // Timeout. Flush the buffer to file
     if (buff_index > 0)
     {
-#if defined(__ENABLE_SDCARD) 
       WriteToFile(buff, buff_index - 1);
-#endif
       file_size += buff_index - 1;
       buff_index = 0;
     }
     Serial.println(".Done");
     Serial.print("Closing file..");
 
-#if defined(__ENABLE_SDCARD) 
     current_file.close();
-#endif
     
     Serial.println("..Ok");
     print_in_progress = false;
 
-#if defined(__ENABLE_LCD)
+
+      char nameBuffer[20];
+      current_file.getName(nameBuffer, 20);
+
+      // Update LCD
+
     // Update LCD
     lcd.clear();
     lcd.print("Done: ");
-#if defined(__ENABLE_SDCARD) 
-    lcd.print(current_file.name());
-#endif
+    lcd.print(nameBuffer);
+
     //TODO: should we look at header and rename file?
-#endif
   } 
 }
 
-#if defined(__ENABLE_SDCARD) 
 void CreateNewFile()
 {  
   // Find unique file 
@@ -275,11 +263,11 @@ void CreateNewFile()
   {
     sprintf (fname, "sa%03d.prn", i);
     i++;
-  } while(SD.exists(fname));
+  } while(sd.exists(fname));
 
   // Found new file
   Serial.println();
-  current_file = SD.open(fname, FILE_WRITE);
+  current_file = sd.open(fname, FILE_WRITE);
   Serial.print("New file created: ");
   Serial.println(fname);
 }
@@ -297,9 +285,7 @@ void WriteToFile(byte* b, int b_size)
     Serial.println("Can't write to file");
   }
 }
-#endif
 
-#if defined(__ENABLE_PARALLEL)
 // Strobe pin on falling edge interrupt
 void StrobeFallingEdge()
 {
@@ -325,4 +311,3 @@ void StrobeFallingEdge()
   // Set ready bit
   data_ready = true;    
 }
-#endif
